e101e66232bc88736134faeb27f05acc7a6c37e3
Knae <general124@hotmail.com>
2021-10-23 01:04:01 +1300

    -creating base classes and system classes -added font file OpenSans

--- CGameManager.cpp
+++ CGameManager.cpp
@@ -1,1 +1,64 @@
 #include "CGameManager.h"
+
+CGameManager::CGameManager()
+{
+	m_v2uGameWindowSize_Current = m_kv2uGameWindowSize_Default;
+
+	m_ptrUIManager = nullptr;
+
+	m_ptrGameWindow = nullptr;
+	m_ptrFont = nullptr;
+	m_ptrSpriteBackground = nullptr;
+}
+
+CGameManager::~CGameManager()
+{
+	delete m_ptrUIManager;
+	m_ptrUIManager = nullptr;
+
+	delete m_ptrGameWindow;
+	m_ptrGameWindow = nullptr;
+
+	delete m_ptrFont;
+	m_ptrFont = nullptr;
+
+	delete m_ptrSpriteBackground;
+	m_ptrSpriteBackground = nullptr;
+}
+
+bool CGameManager::IntializeGame()
+{
+	if (!m_ptrFont->loadFromFile("font/OpenSans-Regular.ttf"))
+	{
+		//ERROR: unable to load font file
+		std::cout << "\nERROR: unable to load font file\n";
+		return false;
+	}
+
+	m_ptrGameWindow = new sf::RenderWindow(sf::VideoMode(m_v2uGameWindowSize_Current.x,m_v2uGameWindowSize_Current.y),"Simple Wars");
+	m_ptrGameWindow->setVerticalSyncEnabled(false);
+	m_ptrGameWindow->setFramerateLimit(30);
+	m_ptrGameWindow->clear();
+
+	return true;
+}
+
+void CGameManager::SetPointersToOtherSystems(CUIManager* _inputUI)
+{
+}
+
+void CGameManager::DrawObject(sf::Drawable* _object)
+{
+}
+
+void CGameManager::DisplayGameWorld()
+{
+}
+
+void CGameManager::DestroyGameWorld()
+{
+}
+
+void CGameManager::DisplayDebug()
+{
+}
--- CGameManager.h
+++ CGameManager.h
@@ -4,12 +4,31 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <iostream>
 #include "CUIManager.h"
 class CGameManager
 {
 public:
+	CGameManager();
+	virtual ~CGameManager();
 
+	virtual bool IntializeGame();
+	virtual void SetPointersToOtherSystems(CUIManager* _inputUI);
+	//void MoveToScene();
+	virtual void DrawObject(sf::Drawable* _object);
+	virtual void DisplayGameWorld();
+	virtual void DestroyGameWorld();
+	virtual void DisplayDebug();
 private:
+	const sf::Vector2u m_kv2uGameWindowSize_Default = sf::Vector2u(1024, 576);
+
+	CUIManager* m_ptrUIManager;
+
+	sf::RenderWindow* m_ptrGameWindow;
+	sf::Font* m_ptrFont;
+	sf::Sprite* m_ptrSpriteBackground;
+
+	sf::Vector2u m_v2uGameWindowSize_Current;
 };
 
 #endif // !__CGAMEMANAGER_H__
